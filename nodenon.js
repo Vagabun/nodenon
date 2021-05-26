59c53348a70a5708cb3e531247c93bad060612e6
Vagabun <16858610+Vagabun@users.noreply.github.com>
2021-05-26 19:43:15 +0200

    feat: add stdout/stderr pipe

--- nodenon.js
+++ nodenon.js
@@ -3,39 +3,31 @@
 const fs =  require('fs');
 const { exec } = require('child_process');
 
-const EXEC_COMMAND = `node ${process.argv[2]}`
+const EXEC_COMMAND = `node ${process.argv[2]}`;
+
 const debounce = (fn, ms) => {
     let debounced = false;
     return () => {
         if (debounced) {
             return;
-        }
+        };
         fn();
         debounced = true;
         setTimeout(() => {
             debounced = false
-        }, ms)
-    }
-}
+        }, ms);
+    };
+};
 const execFn = () => {
-    exec(EXEC_COMMAND, (error, stdout, stderr) => {
-        if (error) {
-            console.log(error);
-            process.exit(0);
-        }
-        if (stderr) {
-            console.log(stderr);
-            process.exit(0);
-        }
-        console.log(stdout);
-    })
-}
+    const execProcess = exec(EXEC_COMMAND);
+    execProcess.stdout.pipe(process.stdout);
+    execProcess.stderr.pipe(process.stderr);
+};
 const debouncedExecFn = debounce(execFn, 3000);
 
 debouncedExecFn();
 fs.watch(`./${process.argv[2]}`, (event) => {
     if (event === 'change') {
-        debouncedExecFn()
-    } 
-})
-
+        debouncedExecFn();
+    }; 
+});
